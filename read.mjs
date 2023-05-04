7f44520ff6178e1505ea2dd9fcd4ce606a450284
Matteo Collina <hello@matteocollina.com>
2023-05-04 10:00:52 +0200

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- read.mjs
+++
@@ -1,7 +0,0 @@
-import { createReadStream } from "fs";
-import { fileURLToPath } from 'node:url'
-const __filename = fileURLToPath(import.meta.url)
-
-// export const stream = createReadStream(__filename);
-
-
